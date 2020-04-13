1b0a08c4e85abca4cdcc530c49dff7b6e799a07f
wojciechlasak <wojciech.lasak@outlook.com>
2020-04-13 11:35:47 +0200

    added routes and methods in controller

--- projekt2/ebiznes_app/app/controllers/HomeController.scala
+++ projekt2/ebiznes_app/app/controllers/HomeController.scala
@@ -20,4 +20,48 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def getProducts: Action[AnyContent] = Action {
+    Ok(views.html.index("Get products"))
+  }
+
+  def getProduct(id: Long): Action[AnyContent] = Action {
+    Ok(views.html.index("Get product"))
+  }
+
+  def deleteProduct(id: Long): Action[AnyContent] = Action {
+    Ok(views.html.index("Delete product"))
+  }
+
+  def addProduct: Action[AnyContent] = Action {
+    Ok(views.html.index("Add product"))
+  }
+
+  def updateProduct: Action[AnyContent] = Action {
+    Ok(views.html.index("Update product"))
+  }
+
+  def getBasket(id: Long): Action[AnyContent] = Action {
+    Ok(views.html.index("Get basket"))
+  }
+
+  def addToBasket: Action[AnyContent] = Action {
+    Ok(views.html.index("Add to basket"))
+  }
+
+  def deleteFromBasket(id: Long): Action[AnyContent] = Action {
+    Ok(views.html.index("Delete from basket"))
+  }
+
+  def getOpinion(id: Long): Action[AnyContent] = Action {
+    Ok(views.html.index("Get opinion"))
+  }
+
+  def addOpinion: Action[AnyContent] = Action {
+    Ok(views.html.index("Add opinion"))
+  }
+
+  def deleteOpinion(id: Long): Action[AnyContent] = Action {
+    Ok(views.html.index("Delete opinion"))
+  }
+
 }
